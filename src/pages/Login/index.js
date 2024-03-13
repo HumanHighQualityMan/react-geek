4da05a13c812dd9b3cf7652573f040654ffa3570
HumanHighQualityMan <178885674@qq.com>
2024-03-13 17:16:28 +0800

    登录页：表单校验和数据收集

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,41 @@
+import "./index.scss";
+import { Button, Form, Input, Card } from "antd";
+import logo from "@/assets//images/logo.png";
+
 const Login = () => {
+	const onFinish = (values) => {
+		console.log("Success:", values);
+	};
 	return (
-		<div>
-			<h1>Login</h1>
+		<div className="login">
+			<Card className="login-container">
+				<img src={logo} className="logo-image" alt="" />
+				<Form onFinish={onFinish} validateTrigger="onBlur">
+					<Form.Item
+						name="username"
+						rules={[
+							{ required: true, message: "请输入手机号" },
+							{ pattern: /^1[3-9]\d{9}$/, message: "请输入正确的手机号" },
+						]}
+					>
+						<Input size="large" placeholder="请输入手机号"></Input>
+					</Form.Item>
+					<Form.Item
+						name="password"
+						rules={[
+							{ required: true, message: "请输入验证码" },
+							{ pattern: /^\d{6}$/, message: "请输入正确的验证码" },
+						]}
+					>
+						<Input size="large" placeholder="请输入验证码"></Input>
+					</Form.Item>
+					<Form.Item>
+						<Button type="primary" size="large" block htmlType="submit">
+							登录
+						</Button>
+					</Form.Item>
+				</Form>
+			</Card>
 		</div>
 	);
 };
